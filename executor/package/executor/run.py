37eb9ed720644ef741cae55aa8f042338772648b
Paweł Pankiewicz <239565@edu.p.lodz.pl>
2022-04-03 13:06:53 +0200

    Temp checkpoint 2

--- executor/package/executor/run.py
+++ executor/package/executor/run.py
@@ -1,25 +1,31 @@
+from math import fabs
+from typing import Dict, Optional, Sequence
 from pyspark import SparkConf
 from pyspark import SparkContext
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
+from pyspark.sql import SparkSession, Row, types as t
+import pyspark.sql.functions as f
+
+
+def toMap(tupleArray: Sequence[Row]) -> Optional[Dict[str, str]]:
+    if tupleArray is None:
+        return
+    return {e["key"].decode("utf-8"): e["value"].decode("utf-8") for e in tupleArray}
+
+
+toMapUdf = f.udf(toMap, t.MapType(t.StringType(), t.StringType()))
 
 conf = (
-    SparkConf()
-    .setMaster("yarn")
-    .setAppName("getClosest")
-    .set("spark.submit.deployMode", "client")
-    .set("spark.yarn.archive", "hdfs://namenode:9000/spark/jars.tar.gz")
+    SparkConf().setAppName("getClosest")
+    # .setMaster("yarn")
+    # .set("spark.submit.deployMode", "client")
+    # .set("spark.yarn.archive", "hdfs://namenode:9000/spark/jars.tar.gz")
 )
 
 sc = SparkContext(conf=conf)
 ss = SparkSession(sc)
-# nodes = ss.read.parquet("hdfs:///data/poland.osm.pbf.node.parquet")
-# filtered1 = nodes.withColumn("tag_count", size(col("tags"))).where(col("tag_count") >= 1)
-# filtered1.write.parquet("hdfs:///temp/hastags.parquet")
-# cleaner = ss.read.parquet("hdfs:///temp/hastags.parquet").where(col("id") == 2003243218)
-# cleaner.write.parquet("hdfs:///temp/cleaner.parquet")
-filtered = ss.read.parquet("hdfs:///temp/hastags.parquet")
-cleaner = ss.read.parquet("hdfs:///temp/cleaner.parquet")
-cleaner.where()
-result = cleaner
-result.show(1, truncate=False)
+nodes = ss.read.parquet("hdfs:///data/poland.osm.pbf.node.parquet")
+nodes.write.parquet("hdfs:///temp/nodesmapped")
+# nodes = ss.read.parquet("hdfs:///temp/nodes5k")
+mapped = nodes.withColumn("tags", toMapUdf(f.col("tags")))
+mapped.printSchema()
+# mapped.where(f.col("tags.name")=="	Cleaner").show(2, truncate=False)
