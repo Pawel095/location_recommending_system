1a9ea18299e0a8152368c9c86a324718ba32d7cb
Paweł Pankiewicz <239565@edu.p.lodz.pl>
2022-04-03 10:21:52 +0200

    Temp backup 1

--- executor/package/executor/run.py
+++ executor/package/executor/run.py
@@ -1,6 +1,7 @@
 from pyspark import SparkConf
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 
 conf = (
     SparkConf()
@@ -12,5 +13,13 @@ conf = (
 
 sc = SparkContext(conf=conf)
 ss = SparkSession(sc)
-nodes = ss.read.parquet("hdfs:///data/poland.osm.pbf.node.parquet")
-print(nodes.count())
+# nodes = ss.read.parquet("hdfs:///data/poland.osm.pbf.node.parquet")
+# filtered1 = nodes.withColumn("tag_count", size(col("tags"))).where(col("tag_count") >= 1)
+# filtered1.write.parquet("hdfs:///temp/hastags.parquet")
+# cleaner = ss.read.parquet("hdfs:///temp/hastags.parquet").where(col("id") == 2003243218)
+# cleaner.write.parquet("hdfs:///temp/cleaner.parquet")
+filtered = ss.read.parquet("hdfs:///temp/hastags.parquet")
+cleaner = ss.read.parquet("hdfs:///temp/cleaner.parquet")
+cleaner.where()
+result = cleaner
+result.show(1, truncate=False)
