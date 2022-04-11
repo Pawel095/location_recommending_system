ef5f0f32cf2d119d3ac9bb9f7fb94f724c849ef5
Paweł Pankiewicz <pawelp6020@gmail.com>
2022-04-11 23:20:37 +0200

    swarm setup

--- prepare.bash
+++ prepare.bash
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+REG_ADDR=192.168.2.1:5000
+
 docker build -t hadoop-base:local_latest ./base
 
 while [ $# -gt 0 ]; do
     case $1 in 
         -d) docker run --rm -it base:latest /bin/bash
+        shift
+        ;;
+        -p) docker tag hadoop-base:local_latest ${REG_ADDR}/hadoop-base:local_latest
+        docker image push ${REG_ADDR}/hadoop-base:local_latest
         shift
         ;;
     esac
