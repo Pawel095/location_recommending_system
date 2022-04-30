c436be55f71b2db83c780e493520553127e6b738
Paweł Pankiewicz <pawelp6020@gmail.com>
2022-04-30 13:10:04 +0200

    installed some management containers

--- executor/execute.bash
+++ executor/execute.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo `env`
-echo alias chrome=\"chromium --no-sandbox --disable-gpu http://namenode:9870 http://historyserver:8188\" > ~/.bashrc
+echo alias chromium=\"chromium --no-sandbox --disable-gpu http://namenode:9870 http://historyserver:8188 \" > ~/.bashrc
 
 sleep infinity
