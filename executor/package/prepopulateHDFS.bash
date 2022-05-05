c12e647f28406fa2ad06711d94ced757e2a62f49
Paweł Pankiewicz <pawelp6020@gmail.com>
2022-05-05 13:19:44 +0200

    add script to write test data to hdfs

--- executor/package/prepopulateHDFS.bash
+++ executor/package/prepopulateHDFS.bash
@@ -6,15 +6,16 @@ JAR_ARCHIVE="jars.tar"
 HDFS_MAP_PATH="/data"
 MAP_FOLDER="map/"
 
-collect_jars
+# collect_jars
+get_test_data
 
 echo Making folders
-hdfs dfs -mkdir -p $HDFS_JARS_PATH
 hdfs dfs -mkdir -p $HDFS_MAP_PATH
+# hdfs dfs -mkdir -p $HDFS_JARS_PATH
 
-hdfs dfs -rm $HDFS_JARS_PATH$JAR_ARCHIVE
-echo Uploading jar archive
-pv $JAR_ARCHIVE | hdfs dfs -put - $HDFS_JARS_PATH$JAR_ARCHIVE
+# hdfs dfs -rm $HDFS_JARS_PATH$JAR_ARCHIVE
+# echo Uploading jar archive
+# pv $JAR_ARCHIVE | hdfs dfs -put - $HDFS_JARS_PATH$JAR_ARCHIVE
 
 # echo Uploading jar files:
 # for file in `ls -A jars`; do
@@ -27,6 +28,6 @@ for file in `ls -A $MAP_FOLDER`; do
     pv $MAP_FOLDER$file | hdfs dfs -put - $HDFS_MAP_PATH/$file
 done
 
-rm -rfv ./ivyJarsCache
-rm -rfv ./jars
-rm -rfv $JAR_ARCHIVE
+# rm -rfv ./ivyJarsCache
+# rm -rfv ./jars
+# rm -rfv $JAR_ARCHIVE
