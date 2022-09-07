9b4c7a22e134d248910af7f9b272f81b0799ec76
Paweł Pankiewicz <pawelp6020@gmail.com>
2022-09-07 12:46:26 +0200

    final fixes before cd

--- hadoop_stack/executor/package/executor/playground.py
+++ hadoop_stack/executor/package/executor/playground.py
@@ -7,6 +7,11 @@ import json
 import shapely
 from geographiclib.geodesic import Geodesic
 
+import pyspark.sql.functions as f
+from pyspark.mllib.recommendation import ALS, Rating
+from pyspark.sql.utils import AnalysisException
+from pprint import pp
+
 conf = (
     g.configure(
         spark_home=fsh(),
@@ -17,16 +22,59 @@ conf = (
     .setAppName("getClosest")
     .set("spark.executor.memory", "15G")
     .set("spark.executor.cores", "16")
+    .set("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0")
 )
 
 from typing import Dict, Optional, Sequence
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession, Row, types as t, DataFrame
 import pyspark.sql.functions as f
-from pyspark.mllib.recommendation import ALS, Rating, MatrixFactorizationModel
 from pprint import pp
 
 HDFS_BASE_ADDRESS = "hdfs:///data_checkpoints/"
+
+
+#%%
+def toMap(key: str, value: str, decode=True) -> Optional[Dict[str, str]]:
+    def __proc(tupleArray: Sequence[Row]):
+        if tupleArray is None:
+            return
+        return {
+            e[key].decode("utf-8")
+            if decode
+            else e[key]: e[value].decode("utf-8")
+            if decode
+            else e[value]
+            for e in tupleArray
+        }
+
+    return __proc
+
+
+tupleUdf = f.udf(toMap("key", "value"), t.MapType(t.StringType(), t.StringType()))
+nodesUdf = f.udf(
+    toMap("index", "nodeId", decode=False), t.MapType(t.IntegerType(), t.LongType())
+)
+#%%
+
+
+def distance_in_meters(p1: shapely.geometry.Point, p2: shapely.geometry.Point):
+    from geographiclib.geodesic import Geodesic
+
+    geodesic = Geodesic.WGS84
+    return geodesic.Inverse(p1.x, p1.y, p2.x, p2.y)["s12"]
+
+
+distance_in_metersUdf = f.udf(distance_in_meters, t.DoubleType())
+
+
+#%%
+
+
+def drop_columns_and_mappify_tags(df: DataFrame) -> DataFrame:
+    return df.drop("uid", "user_sid", "changeset", "version").withColumn(
+        "tags", tupleUdf(f.col("tags"))
+    )
 
 
 def repl():
@@ -36,56 +84,60 @@ def repl():
 
 
 s = SparkSession.builder.config(conf=conf).getOrCreate()
+s.sparkContext.setLogLevel("INFO")
 s.sparkContext.setCheckpointDir("hdfs:///spark_checkpoints")
-# s.sparkContext.setLogLevel("INFO")
-# s.sparkContext.setLogLevel("WARN")
 
 g.init_sql(s)
 pp(s.sparkContext.getConf().getAll())
+COMBINED_TAG_FILTER = (
+    f.col("tags.amenity").isin(
+        [
+            "bar",
+            "biergarten",
+            "cafe",
+            "fast_food",
+            "food_court",
+            "ice_cream",
+            "pub",
+            "restaurant",
+            "arts_centre",
+            "casino",
+            "cinema",
+            "community_centre",
+            "conference_centre",
+            "events_venue",
+            "pharmacy",
+            "planetarium",
+            "studio",
+            "gym",
+            "internet_cafe",
+        ]
+    )
+    | f.col("tags.leisure").isin(
+        [
+            "adult_gaming_centre",
+            "amusement_arcade",
+            "beach_resort",
+            "bowling_alley",
+            "disc_golf_course",
+            "dog_park",
+            "escape_game",
+            "fitness_centre",
+            "golf_course",
+            "hackerspace",
+            "ice_rink",
+            "miniature_golf",
+            "resort",
+            "sports_centre",
+            "sports_hall",
+            "swimming_area",
+            "swimming_pool",
+            "water_park",
+        ]
+    )
+    | f.col("tags.shop").isNotNull()
+) & f.col("tags.name").isNotNull()
 
 
-def build_and_test(r, model):
-    testdata = r.map(lambda p: (p[0], p[1]))
-    predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
-    ratesAndPreds = r.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
-    MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1]) ** 2).mean()
-    print("Mean Squared Error = " + str(MSE))
-
-
-#%%
-
+s.sparkContext.setLogLevel("ERROR")
 repl()
-
-all_names = (
-    s.read.parquet(HDFS_BASE_ADDRESS + "/n_filtered")
-    .withColumn("name", f.col("tags.name"))
-    .select("name")
-    .distinct()
-    .withColumn("count", f.lit(0))
-    .withColumnRenamed("name", "aname")
-    .withColumn("nid", f.monotonically_increasing_id())
-)
-all_names.write.parquet("/recomender_name_id_map")
-
-rank_df = (
-    s.read.parquet("/recommender_data")
-    .select(["nid", "ntags", "npoint"])
-    .withColumn("name", f.col("ntags.name"))
-    .groupBy("name")
-    .count()
-    .orderBy("count", ascending=False)
-    .alias("c")
-    .join(all_names.alias("an"), f.col("c.name") == f.col("an.aname"), "right")
-    .selectExpr(["c.count as count", "nid as id"])
-    .fillna(0)
-    .orderBy("count", ascending=False)
-    .withColumn("user", f.lit(0))
-)
-
-r = rank_df.rdd.map(lambda row: Rating(row[2], row[1], row[0]))
-repl()
-
-a = ALS.train(r, 10, 10)
-build_and_test(r, a)
-a.save(s.sparkContext, "/als_built")
-
